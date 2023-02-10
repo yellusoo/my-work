bf5e99012f16e914c64088eeb46940c8a7bf5307
unknown <oosulley@gmail.com>
2023-02-10 12:27:18 +0000

    First program, Lab 3.3

---
+++ Week3/length.py
@@ -0,0 +1,7 @@
+#This program is the 1st program of Lab 3.3
+#This program reads a string and outputs it's length
+#Author: OOS
+
+stringy = str(input("Please enter a word here:"))
+string_length = len (stringy)
+print(" The string is {} characters".format(string_length))
