8b2e1c0d3ed56db575084f7b768e7a228bda986c
unknown <oosulley@gmail.com>
2023-02-10 12:07:29 +0000

    2nd Program, Lab 3.2

---
+++ Week3/absolute.py
@@ -0,0 +1,7 @@
+#This program follows Lab 3.2
+#This program takes a number and gives its absolute value
+#Author: OOS
+
+number = float(input("Enter a number:"))            
+absol = abs(number)
+print ("The absolute value of {} is {}".format(number,absol))
