d18cc8eaf3b0a78ca9e71f4f6204af49aa76da4d
unknown <oosulley@gmail.com>
2023-02-10 11:21:47 +0000

    Program 4, Lab 3.1

---
+++ Week3/random_generator.py
@@ -0,0 +1,7 @@
+#Program prints a random number between 1 and 10
+#AUthor: OOS
+
+import random
+
+number = random.randint(1,10)
+print("this is a random number between one and ten: {}".format(number))
