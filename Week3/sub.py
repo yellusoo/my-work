9ec5c7ca2281e40a454a880766de18a3eb518642
unknown <oosulley@gmail.com>
2023-02-10 11:03:13 +0000

    2nd program as part of Lab 3.1

---
+++ Week3/sub.py
@@ -0,0 +1,8 @@
+#This is the 2nd program as part of Lab 3.1
+#This program reads in two numbers and does subtraction
+#Author: OOS
+
+first_number = int(input("Input your first number: "))
+second_number = int(input("Input your second number: "))
+answer = first_number - second_number
+print("{} minus {} is {}".format(first_number, second_number,answer))
