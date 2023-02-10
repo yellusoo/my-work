aa9f1dfb2cf25f8efd8ed48d13276a927d77b9cd
unknown <oosulley@gmail.com>
2023-02-10 10:49:44 +0000

    Week 3, Lab 1 First program

---
+++ Week3/test_types.py
@@ -0,0 +1,16 @@
+#THis program follows Lab 3.1
+#Author: OOS
+
+var_i = 3
+var_fl = 3.5
+var_bool = True
+var_memo = "How now brown cow"
+var_list = []
+
+# These following lines are taken directly from the lab sheet 3.1 and adapted for my variable names
+
+print('variable {} is of type:{} and value:{}'.format('i', type(var_i), var_i))
+print('variable {} is of type:{} and value:{}'.format('var_fl', type(var_fl), var_fl))
+print('variable {} is of type:{} and value:{}'.format('var_bool', type(var_bool), var_bool))
+print('variable {} is of type:{} and value:{}'.format('var_memo', type(var_memo), var_memo))
+print('variable {} is of type:{} and value:{}'.format('var_list', type(var_list), var_list))
