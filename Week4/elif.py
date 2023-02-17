0a173cac5f2666ffff14f22429df74edc69c3c9a
unknown <oosulley@gmail.com>
2023-02-17 14:39:19 +0000

    n

--- Week4/elif.py
+++ Week4/elif.py
@@ -8,3 +8,9 @@ if(grade < 40):
     print("you have a fail")
 if grade >= 40 and grade <=50 :
     print("You have a D")
+
+c = 34
+if c == 1:
+    print (f"c is {c}")
+else:
+ print (f"c is equal to {c}")
