8771e9d2a9198d7916d1ff1841fcdb52721727bf
unknown <oosulley@gmail.com>
2023-02-17 19:54:02 +0000

    Week 4 Weekly task

---
+++ Week4/for_loops.py
@@ -0,0 +1,18 @@
+# 2nd program of Lecture 2 week 4
+#This program tests for loops
+#Author: OOS
+
+boats = ["signa", "yacht", "swan"]
+
+for boat in boats:
+    print(" I'd rather be sailing in my", boat)
+
+print(boat)
+
+for i in range(1,56):
+    print(i)
+
+print("All done, i is equal to", i)
+
+if "swan" in boats:
+    print("Yay boat", boat)
