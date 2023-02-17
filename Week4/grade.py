8989b9933162f12aeb72c87006b93f372da3f918
unknown <oosulley@gmail.com>
2023-02-17 15:44:36 +0000

    Lab 1 Program 2 - Grades and Results

---
+++ Week4/grade.py
@@ -0,0 +1,21 @@
+#Week 4, Lecture 1 2nd program
+#This program checks a students grade and reports their result
+#Author: OOS
+
+grade = float(input("Input your grade here:"))
+
+grade2 = round(grade)
+
+if grade2 < 0 or grade >100:
+    print("Error; Try again")
+
+if grade2 < 40:
+    print("Fail")
+if grade2 >=40 and grade <=49:
+    print("Pass")
+if grade2 >49 and grade <=59:
+    print("Merit 2")
+if grade2 >59 and grade <=69:
+    print("Merit 1")
+if grade2 >=70 and grade < 100:
+    print("Distinction")
---
+++ Week4/is_even.py
@@ -0,0 +1,9 @@
+#This file is the first program of the first lab as part of week 4
+#This program checks if a number is odd or even
+#Author: OOS
+
+number = int(input("please enter a number here: "))
+if number % 2   == 0:
+    print("Even")
+else:
+    print("Odd")
