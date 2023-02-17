470fa46bf5885633f73e1a3fc590cc072f329ac9
unknown <oosulley@gmail.com>
2023-02-17 14:31:28 +0000

    if and else and elif statements rough work

---
+++ Week4/elif.py
@@ -0,0 +1,10 @@
+#Following Lecture 1 of Week 4
+#Messing around with if, elif statements
+#Author: OOS
+
+grade = 45
+
+if(grade < 40):
+    print("you have a fail")
+if grade >= 40 and grade <=50 :
+    print("You have a D")
