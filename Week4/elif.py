ed774412bba42d79ba877fff6c90062598e00203
unknown <oosulley@gmail.com>
2023-02-17 14:53:26 +0000

    Week 4 Lecture 1 Messing

--- Week4/elif.py
+++ Week4/elif.py
@@ -10,7 +10,16 @@ if grade >= 40 and grade <=50 :
     print("You have a D")
 
 c = 34
-if c == 1:
+if isinstance (c, int) and c == 1:
     print (f"c is {c}")
 else:
  print (f"c is equal to {c}")
+
+ d = 4
+if d<0:
+    print(f"D is less than 0")
+elif d>15:
+    print("d is positive")
+else:
+    if d>0 and d<10:
+        print("D is between 0 and 15")
