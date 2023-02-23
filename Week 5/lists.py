59efe32df2bc160469797dbe0e4a8b229b4e29cf
unknown <oosulley@gmail.com>
2023-02-23 16:19:10 +0000

    Commit Week 5

---
+++ Week 5/lists.py
@@ -0,0 +1,25 @@
+#This file follows Lecture 1 on lists and tuples
+#Author: OOS
+'''
+boats = ['Sigma', 23, [1,2,3], {}]
+
+boats = boats + [1,2,3]
+
+print(boats)
+
+print(len(boats))
+
+for boat in boats:
+    print(type(boats))
+
+
+sum = 0
+ages = [12,24,23,31]
+
+for age in ages:
+    sum += age 
+print (sum)
+'''
+
+string = "1234567890"
+print(string [-7: ])
---
+++ Week 5/tuples.py
@@ -0,0 +1,10 @@
+#Following Lecture 1 Part 2
+#Author: OOS
+
+#Cant set or append tuples. Tuples pass back multiple things to a function
+
+t = (1,2,3)
+
+x,y,z = t #Can assign variables names to values in tuple
+
+print(x,y,z)
